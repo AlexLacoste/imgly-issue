92b67146af45d108598662a72811b77cb62b078a
Alexandre Lacoste <alexlacoste3@gmail.com>
2024-11-22 10:46:21 +0100

    add issue exportation with no modifications

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
 import { Button, StyleSheet, View } from 'react-native';
-import { Configuration, ForceTrimMode, SerializationExportType, VESDK } from 'react-native-videoeditorsdk';
+import { Configuration, ForceTrimMode, VESDK, VideoCodec } from 'react-native-videoeditorsdk';
 import React, {useRef, useState, useCallback, useEffect} from 'react';
 import { Camera, useCameraDevice, useCameraPermission } from 'react-native-vision-camera';
 import * as FileSystem from 'expo-file-system';
 
-
-const getVESDKCongif =  (): Configuration => {
+const getVESDKConfig = (): Configuration => {
   const options: Configuration = {
     composition: {
       personalVideoClips: true,
@@ -15,84 +14,53 @@ const getVESDKCongif =  (): Configuration => {
       forceMode: ForceTrimMode.IF_NEEDED,
     },
     export: {
-      serialization: {
-        enabled: true,
-        exportType: SerializationExportType.OBJECT,
-      },
+
       video: {
-        segments: true,
+        codec: VideoCodec.HEVC,
+        quality: 0.8,
       },
-      filename: FileSystem.cacheDirectory +
-      `export${new Date().getTime()}`,
+      filename:
+        FileSystem.cacheDirectory + `export${new Date().getTime()}`,
     },
   };
 
   return options;
 };
 
-
 export default function App() {
-  const device = useCameraDevice("back");
+  const device = useCameraDevice('back');
   const { hasPermission, requestPermission } = useCameraPermission();
   const cameraRef = useRef<Camera>(null);
   const [recording, setRecording] = useState(false);
-  const [segments, setSegments] = useState(null);
-  const [serialization, setSerialization] = useState(undefined);
-
-  const openAgainEditor = useCallback(async () => {
-    const vesdkConfig =  getVESDKCongif();
-
-    // Since the 2nd segment is deleted, we are unable to open the editor again
-    await VESDK.openEditor(segments, vesdkConfig, serialization);
-  }, [serialization, segments]);
 
   const onPress = useCallback(() => {
     if (!recording) {
       setRecording(true);
       cameraRef.current?.startRecording({
         onRecordingFinished: async (video) => {
-          const videos = [video];
           setRecording(false);
+          // do less than 120 seconds
+          console.log('Video duration:', video.duration);
+          console.log('Video path:', video.path);
           try {
-            if (!Array.isArray(videos))
-              throw new Error("ERROR: videos should be an array");
-            if (!videos?.length)
-              throw new Error(
-                "ERROR: videos array should not be empty"
-              );
-            const videoClips = videos.map(
-              (video: any, index: number) => {
-                return {
-                  identifier: index.toString(),
-                  videoURI: video?.sourceURL ?? video?.path,
-                };
+            const videoClips = [
+              {
+                identifier: 0,
+                videoURI: video?.path,
               }
-            );
-            const vesdkConfig =  getVESDKCongif();
+            ]
+            const vesdkConfig = getVESDKConfig();
 
-
-            // Open the video editor with a record, after in the editor add a new video from your gallery and export the video, we should get 2 segments
-
+            // Open the video editor with the recorded video and just export it with no modifications
             const result = await VESDK.openEditor(
               videoClips,
               vesdkConfig
             );
 
-            if (result) {
-              // The 2 segment's uri is already inexistent or deleted
-              console.log("segments", result?.segments);
-              setSegments( result?.segments);
-              setSerialization(result?.serialization);
-
-              const is2ndSegmentAvailable = await FileSystem.getInfoAsync(
-                // @ts-ignore
-                result.segments[1].videoURI.toString()
-              );
-              // The second segment doesn't exist
-              console.log("second segment info", is2ndSegmentAvailable);
-            }
+            // a new video was created even if there was no changes
+            console.log('result video open editor:', result.video); 
           } catch (error) {
-            console.log('error',error);
+            console.log('Error:', error);
           }
         },
         onRecordingError: (error) => console.error(error),
@@ -106,7 +74,7 @@ export default function App() {
     if (!hasPermission) requestPermission();
   }, [hasPermission]);
 
-  if (!hasPermission || !device) return <View/>;
+  if (!hasPermission || !device) return <View />;
 
   return (
     <>
@@ -119,25 +87,19 @@ export default function App() {
       />
       <View
         style={{
-          backgroundColor: recording ? "red" : "blue",
-          position: "absolute",
+          backgroundColor: recording ? 'red' : 'blue',
+          position: 'absolute',
           bottom: 50,
           width: 500,
-          alignSelf: "center",
-          justifyContent: "center",
-          alignItems: "center",
+          alignSelf: 'center',
+          justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
-        <Button
-          title="record"
-          onPress={onPress}
-        />
+        <Button title={recording ? 'Stop Recording' : 'Start Recording'} onPress={onPress} />
 
-        {segments && serialization && (
-          <Button title="open again editor" onPress={openAgainEditor} />
-        )}
+
       </View>
     </>
   );
 }
-
